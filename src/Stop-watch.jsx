6f3f3294014815b57622fd0516793e3c2408b0b7
Sukhankar <sukhankarsh@gmail.com>
2024-06-21 21:33:12 +0530

    added the functions to the buttons

--- src/Stop-watch.jsx
+++ src/Stop-watch.jsx
@@ -9,27 +9,48 @@ function Stopwatch(){
     const startTimeRef = useRef(0);
 
     useEffect(() =>{
+        if(isRunning){
+            intervalIdRef.current = setInterval(()=>{
+                setElapsedTime(Date.now() - startTimeRef.current);
+            }, 10 )
+        }
+        return ()=>{
+            clearInterval(intervalIdRef.current);
+        }
 
     }, [isRunning])
 
     function start(){
-
+        setIsRunning(true);
+        startTimeRef.current = Date.now() - elapsedTime;
     }
 
     function stop(){
-
+        setIsRunning(false);
     }
 
     function reset(){
-         
+         setElapsedTime(0);
+         setIsRunning(false);
     }
 
     function fromatTime(){
 
-        return `00:00:00`;
+        let hours = Math.floor(elapsedTime /(1000 * 60 * 60));
+        let minutes = Math.floor(elapsedTime /(1000 * 60)%60);
+        let seconds = Math.floor(elapsedTime /(1000)%60);
+        let milliseconds = Math.floor((elapsedTime %1000) / 10);
+
+        hours = String(hours).padStart(2, "0");
+        minutes = String(minutes).padStart(2, "0");
+        seconds = String(seconds).padStart(2, "0");
+        milliseconds = String(milliseconds).padStart(2, "0");
+
+        return `${hours}:${minutes}:${seconds}:${milliseconds}`;
     }
 
-    return(
+    return(<>
+        <h1>Stop Watch</h1>
         <div className="stopwatch">
             <div className="display">  {fromatTime()}   </div>
             <div className="controls">
@@ -40,6 +61,7 @@ function Stopwatch(){
             
 
         </div>
+        </>
     )
 }
 
